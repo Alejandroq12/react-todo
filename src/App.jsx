79d8f416b58ec265d148c4a91d5f9b6391c23230
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-01 13:25:35 -0600

    Set started files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '../../../../../../../../../vite.svg';
 import './App.css';
 
 function App() {
@@ -9,9 +8,6 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
