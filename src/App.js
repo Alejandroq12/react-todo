0f552fb8ef8f67277a0b69ed5822aa92eab7ad0b
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-01 13:16:46 -0600

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button type="submit" onClick={() => setCount((count) => count + 1)}>
           count is
           {' '}
           {count}
