5ce3afe598c381416aa18a0df917205a1e13f041
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-01 14:01:49 -0600

    Add main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import TodoApp from './components/TodoApp';
+
+import './styles/app.css';
+
+const domContainer = document.getElementById('root');
+const root = ReactDOM.createRoot(domContainer);
+root.render(
   <React.StrictMode>
-    <App />
+    <TodoApp />
   </React.StrictMode>,
 );
