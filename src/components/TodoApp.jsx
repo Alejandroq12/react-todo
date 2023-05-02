2ed4f34fdc278d7a4f4108eed10d2d2d49a180f8
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-02 13:44:01 -0600

    Update main componente to render TodosLogic

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable import/extensions */
-import Header from '@/components/Header.jsx';
+import Header from '@/components/Header';
+import TodosLogic from '@/components/TodosLogic';
 
 const TodoApp = () => (
   <>
     <Header />
+    <TodosLogic />
   </>
 );
 export default TodoApp;
