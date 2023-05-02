c6279b676c32cad648c8d6b487066680ea2610c4
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-02 15:04:05 -0600

    Add hardcoded array

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,4 +1,30 @@
-const TodosLogic = () => (
-  <div>TodosLogic content</div>
-);
+const TodosLogic = () => {
+  const todos = [
+    {
+      id: 1,
+      title: 'Setup development enviroment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Developed website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ];
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li>
+          {todo.title}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
 export default TodosLogic;
