91cc1599cdca496ee5789dc3d16c37e8d9aa6902
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-02 11:27:23 -0600

    Add main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import TodoApp from './components/TodoApp';
+
+const domContainer = document.getElementById('root');
+const root = ReactDOM.createRoot(domContainer);
+root.render(<TodoApp />);
